1404b5467b948ae1aabcf1da0345bc1ac100b78f
Cutie <cutie@anbos-mac-mini.lan>
2019-04-23 01:54:04 -0400

    Updates

---
+++ precache-manifest.78d42bf4328946245958115cbb6f9290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b8640790262b144e7170de8d043153",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1b723c52762e2818020",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "283cbb3238da73526ab7",
+    "url": "/static/js/2.21e67013.chunk.js"
+  },
+  {
+    "revision": "a1b723c52762e2818020",
+    "url": "/static/js/main.a06c21ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
